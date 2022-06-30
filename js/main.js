2173dc3712281256b9b7234f54ac43e191ea1707
Jonas Kwiedor <mail@gowizzard.de>
2022-06-30 12:41:06 +0200

    Update javascript file

--- js/main.js
+++ js/main.js
@@ -3,25 +3,22 @@ setInterval(function() {
     document.querySelector("#current-time").innerHTML = new Date().toTimeString()
 }, 1);
 
-// Load chrome storage
+// Load chrome storage & check if the ip is already stored
+// Then check the ip address with ipify.org, stored the response & write the response to inner html
 chrome.storage.sync.get((data) => {
 
-    // Check if ip address is stored
     if (data.ipAddres) {
         document.querySelector("#ip-address").innerHTML = data.ipAddress
     }
 
-    // Check ip address with ipify.org
     fetch('https://api.ipify.org')
         .then(response => response.text())
         .then(data => {
 
-            // Save ip address to chrome storage
             chrome.storage.sync.set({
                 ipAddress: data
             })
 
-            // Write the ip address to the page
             document.querySelector("#ip-address").innerHTML = data
 
         })
