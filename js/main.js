a9adc3b2f801fe527eff5ac1d55ad9d028ff33c9
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-06-30 11:25:40 +0200

    Add first plugin files

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// Check the current time
+setInterval(function() {
+    document.querySelector("#current-time").innerHTML = new Date().toTimeString()
+}, 1);
+
+// Load chrome storage
+chrome.storage.sync.get((data) => {
+
+    // Check if ip address is stored
+    if (data.ipAddress.length > 0) {
+        document.querySelector("#ip-address").innerHTML = data.ipAddress
+    }
+
+    // Check ip address with ipify.org
+    fetch('https://api.ipify.org')
+        .then(response => response.text())
+        .then(data => {
+
+            // Save ip address to chrome storage
+            chrome.storage.sync.set({
+                ipAddress: data
+            })
+
+            // Write the ip address to the page
+            document.querySelector("#ip-address").innerHTML = data
+
+        })
+
+})
